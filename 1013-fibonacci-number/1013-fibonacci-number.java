f60339af9afab3a2fdb7b72650d27c173bb70ff9
Dhatri <156882180+Dhatri0612@users.noreply.github.com>
2025-05-22 16:57:24 +0530

    Time: 0 ms (100%), Space: 40.1 MB (96.36%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }else{
+            int first_number=0;
+            int second_number=1;
+            for(int i=1;i<=n;i++){
+                int third_number= first_number+second_number;
+                first_number=second_number;
+                second_number=third_number;
+            }
+            return first_number;
+        }
+    }
+}
